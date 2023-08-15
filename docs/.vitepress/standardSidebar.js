d70014d51012bac937d77e40fc4e1a9c13d1786e
wangming <frontend_ming@163.com>
2023-08-15 10:07:06 +0800

    MOD-STYLE:整理文档

--- docs/.vitepress/standardSidebar.js
+++ docs/.vitepress/standardSidebar.js
@@ -9,7 +9,6 @@ export default [
             { text: 'Commit 信息规范', link: `${commonPath}/git/commit.md` },
             { text: '常用 Git 命令清单', link: `${commonPath}/git/handle.md` },
             { text: 'Flow 工作流', link: `${commonPath}/git/flow.md` },
-            { text: 'Git 使用实战', link: `${commonPath}/git/practice.md` },
         ]
     },
     {
